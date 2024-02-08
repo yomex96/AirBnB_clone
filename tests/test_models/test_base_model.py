3911d8eb95f7a690f421cf0a3936e712e99abe6e
yomex96 <abayomirobertonawole2@gmail.com>
2024-02-08 03:52:00 -0800

    list

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -13,7 +13,6 @@ from datetime import datetime
 from time import sleep
 from models.base_model import BaseModel
 
-
 class TestBaseModel_instantiation(unittest.TestCase):
     """The Unittests for testing instantiation of the BaseModel class."""
 
