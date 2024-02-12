3ab8cdb8c8727bab4d173094d695d62f5d242db0
yomex96 <abayomirobertonawole2@gmail.com>
2024-02-11 22:53:12 -0800

    swart

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,7 +13,7 @@ class FileStorage:
 
     def all(self):
         """returns the dictionary __objects"""
-        return FileStorage.__objects
+        return type(self).__objects
 
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
@@ -46,50 +46,53 @@ class FileStorage:
         return classes
 
     def reload(self):
-        """Reloads the stored objects"""
-        if not os.path.isfile(FileStorage.__file_path):
-            return
-        with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
-            obj_dict = json.load(f)
-            obj_dict = {k: self.classes()[v["__class__"]](**v)
-                        for k, v in obj_dict.items()}
-            # TODO: should this overwrite or insert?
-            FileStorage.__objects = obj_dict
+    """Reloads the stored objects"""
+    if not os.path.isfile(FileStorage.__file_path):
+        return
+    with open(FileStorage.__file_path, "r", encoding="utf-8") as f:
+        obj_dict = json.load(f)
+        for k, v in obj_dict.items():
+            class_name = v["__class__"]
+            if class_name in self.classes():
+                obj_dict[k] = self.classes()[class_name](**v)
+        FileStorage.__objects = obj_dict
+
 
     def attributes(self):
-        """Returns the valid attributes and their types for classname"""
-        attributes = {
-            "BaseModel":
-                     {"id": str,
-                      "created_at": datetime.datetime,
-                      "updated_at": datetime.datetime},
-            "User":
-                     {"email": str,
-                      "password": str,
-                      "first_name": str,
-                      "last_name": str},
-            "State":
-                     {"name": str},
-            "City":
-                     {"state_id": str,
-                      "name": str},
-            "Amenity":
-                     {"name": str},
-            "Place":
-                     {"city_id": str,
-                      "user_id": str,
-                      "name": str,
-                      "description": str,
-                      "number_rooms": int,
-                      "number_bathrooms": int,
-                      "max_guest": int,
-                      "price_by_night": int,
-                      "latitude": float,
-                      "longitude": float,
-                      "amenity_ids": list},
-            "Review":
-            {"place_id": str,
-                         "user_id": str,
-                         "text": str}
-        }
-        return attribute
+    """Returns the valid attributes and their types for classname"""
+    attributes = {
+        "BaseModel":
+                 {"id": str,
+                  "created_at": datetime.datetime,
+                  "updated_at": datetime.datetime},
+        "User":
+                 {"email": str,
+                  "password": str,
+                  "first_name": str,
+                  "last_name": str},
+        "State":
+                 {"name": str},
+        "City":
+                 {"state_id": str,
+                  "name": str},
+        "Amenity":
+                 {"name": str},
+        "Place":
+                 {"city_id": str,
+                  "user_id": str,
+                  "name": str,
+                  "description": str,
+                  "number_rooms": int,
+                  "number_bathrooms": int,
+                  "max_guest": int,
+                  "price_by_night": int,
+                  "latitude": float,
+                  "longitude": float,
+                  "amenity_ids": list},
+        "Review":
+        {"place_id": str,
+                     "user_id": str,
+                     "text": str}
+    }
+    return attributes  # Corrected typo: return attributes, not attribute
+
